55ff58d77a615590b9eed10bc5caff528f05fef5
Talia Glantz <taliaglantz13@gmail.com>
2021-10-27 18:18:58 +0100

    updating

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
 const Home = () => {
 
   return (
     <section className="hero is-fullheight-with-navbar is-danger" id="curtains">
       <div className="hero-body">
-
         <div className="container">
-
-          <figure className="image is-3by2">
-            <img src="assets/curtain-17437.png"></img>
-            <h1 className="title is-1 has-text-centered">
-              <span className="logo-emoji" role="img" aria-label="logo">🎬 </span>
-              <Link className="has-text-white" id="enter" to="/movies">Enter</Link>
-              <span className="logo-emoji" role="img" aria-label="logo"> 🎬</span>
-            </h1>
-          </figure>
-
+          {/* <figure className="image is-3by2"> */}
+          {/* <img src="assets/curtain-17437.png"></img> */}
+          <h1 className="title is-1 has-text-centered">
+            <span className="logo-emoji" role="img" aria-label="logo">🎬 </span>
+            <Link className="has-text-white" id="enter" to="/movies">Enter</Link>
+            <span className="logo-emoji" role="img" aria-label="logo"> 🎬</span>
+          </h1>
+          {/* </figure> */}
         </div>
-
       </div>
-    </section >
+    </section>
   )
-
-
-
-
-
 }
 
 export default Home
+
+
+
+
+
+
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 const MovieCard = ({ id, rank, title, image }) => {
   // console.log('id ->', id)
 
@@ -8,19 +7,19 @@ const MovieCard = ({ id, rank, title, image }) => {
     <div key={id} className="column is-one-quarter-desktop is-one-third-tablet">
       <Link to={`/movies/${id}`}>
         <div className="card">
-          <div className="card-header">
-            <div className="card-subtitle title is-5">{rank}</div>
-            <div className="card-header-title title is-4">{title}</div>
-          </div>
-          <figure className="image image-is-1by1">
+          <figure className="image image-is-3by4">
             <img src={image} alt={title}></img>
           </figure>
+          <div className="card-header">
+            
+            <div className="card-subtitle title is-7">{rank}
+              <div className="card-header-title title is-7">{title}</div>
+            </div>
+          </div>
+
         </div>
       </Link>
     </div>
-
-
   )
 }
-
 export default MovieCard
