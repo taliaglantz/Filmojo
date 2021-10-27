76d36c7badf2040200cc7bac040e4733e3d121e3
Talia Glantz <taliaglantz13@gmail.com>
2021-10-27 10:46:32 +0100

    updating10:46Wed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import React from 'react'
-
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+// import Home from './components/Home'
 
 const App = () => {
 
- 
-  
+  const [movies, setMovies] = useState([])
+  const [hasError, setHasErrors] = useState(false)
+
+  useEffect(() => {
+    const getData = async () => {
+
+      try {
+        const { data } = await axios.get('https://imdb-api.com/en/API/Top250Movies/k_0vc4n4g6')
+        setMovies(data.items)
+      } catch (err) {
+        setHasErrors(true)
+      }
+    }
+    getData()
+  }, [])
+
+  //console.log('movies on state->', movies)
   return (
     <>
-      <h1>Project 2</h1>
+      <section className="section">
+        <div className="container">
+          {movies.length ?
+            <div className="columns is-multiline">
+              {movies.map(movie => {
+                return (
+                  <p key={movie.id}>{movie.title}</p>
+                )
+
+                // console.log(movie.id)
+
+
+
+              })}
+
+            </div>
+            :
+            <h2 className="title has-text-centered">
+              {hasError ? 'Something has gone wrong!' : 'loading...'}
+            </h2>
+          }
+        </div>
+      </section>
     </>
   )
 }
+
 
 export default App
---
+++ src/Wireframe.js
@@ -0,0 +1,55 @@
+// Movies/Netflix style film list
+
+// Enter screen showing user going into the cinema
+
+// Once in cinema, display all top 250 movies in cheesebored/tile style
+// this API is the 250 top movies one
+
+// click on movie and it takes you to that movie’s page
+// this is a different API which takes in the movies’ ID
+// full description (plot, actors, directors etc.) on movie page
+// similar movies also displayed as cards at bottom - ability to click on these too - take you to that movie’s page
+// ability to “favourite” a movie - this will get added ot favourites list
+
+// NavBar
+// Movie roulette button -> this would take you to a random movie’s page
+// Favourite films page
+// Get filtering to work - genre, directors etc.
+
+// Styling
+// Curtains upon entering cinema
+// 1920s theme
+// art deco style
+
+// Day-to-day planning
+
+// Wednesday
+
+// Day
+
+// build out movie card
+// use same bulma for now then change it all later?
+// move movie card to seperate file
+// move MovieIndex to seperate file
+// create home file
+// create routes in app.js
+// ensure links are all set up
+
+// Eve
+
+// Bulma and design
+
+// Thursday
+
+// Day
+
+// Display similar movies as cards at bottom
+// Ability to favourite certain movies and add to list
+// random movie generator
+// filtering
+
+// Eve
+
+// Bulma
+
+
